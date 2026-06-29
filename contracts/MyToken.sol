1be465b30556b7b7fabe39844a4118c4d20218ee
doa Kwon <doakwon1120@gmail.com>
2026-06-29 18:10:10 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,11 +9,11 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        _mint(1 * 10 ** uint256(decimals), msg.sender); // 1 MT
+        _mint(_amount * 10 ** uint256(decimals), msg.sender);
     }
     
     function _mint(uint256 amount, address owner) internal {
@@ -21,15 +21,10 @@ contract MyToken {
         balanceOf[owner] = balanceOf[owner] + amount;
     }
 
-    // function totalSupply() external view returns (uint256) {
-    //     return totalSupply;
-    // }
-
-    // function balanceOf(address owner) external view returns (uint256) {
-    //     return balanceOf[owner];
-    // }
-
-    // function name()  external view returns (string memory) {
-    //    return name;
-    // }
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
 }
