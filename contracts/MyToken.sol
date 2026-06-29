dfd860732d910ac04cdbaf467281856964198d06
doa Kwon <doakwon1120@gmail.com>
2026-06-29 15:03:56 +0900

    mint function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -13,8 +13,14 @@ contract MyToken {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
+        _mint(1 * 10 ** uint256(decimals), msg.sender); // 1 MT
     }
     
+    function _mint(uint256 amount, address owner) internal {
+        totalSupply = totalSupply + amount;
+        balanceOf[owner] = balanceOf[owner] + amount;
+    }
+
     // function totalSupply() external view returns (uint256) {
     //     return totalSupply;
     // }
@@ -22,7 +28,7 @@ contract MyToken {
     // function balanceOf(address owner) external view returns (uint256) {
     //     return balanceOf[owner];
     // }
-    
+
     // function name()  external view returns (string memory) {
     //    return name;
     // }
